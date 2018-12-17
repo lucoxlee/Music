2abe1cd5ab5740aea43c41f28f3a9451a311f468
yule <lucoxlee@126.com>
2018-12-17 15:55:05 +0800

    do what you want

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from __init__ import create_app
 
+app = create_app()
 if __name__ == '__main__':
-    create_app().run()
+    app.run()
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
+        'flask', 'tornado'
     ],
     extras_require={
         'test': [
