951ac53a9a516de2ab8629c1df9a28bac18a2d4b
yule <lucoxlee@126.com>
2018-12-17 13:04:50 +0800

    do what you want

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+from __init__ import create_app
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
